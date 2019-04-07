b8a92fdc5071d8ecba9d04f84d7e4c57c0620fd7
projectAnand <46249362+projectAnand@users.noreply.github.com>
2019-04-07 21:07:43 +0300

    Update serial_test.py

--- serial_test.py
+++ serial_test.py
@@ -3,12 +3,43 @@
 #
 #
 
+
 import serial as sr
 
 ARDUINO_OK = 10
 RASPBERRY_OK = 11
 
 READ_SIGN = 19
+
+
+###  FUNCTION DEFINITIONS
+
+
+def read_sign():
+	
+	#read your sign here 
+	
+	
+	outputData = A1   ##write a type from  '### OUTPUT TYPES'
+	
+	ser.write(outputData)
+	
+	return outputData
+
+### OUTPUT TYPES
+
+LEFT = b'L'
+RIGHT = b'R'
+FORWARD = b'F'
+A1 = b'1'
+A2 = b'2'
+A3 = b'3'
+B1 = b'4'
+B2 = b'5'
+B3 = b'6'
+
+### Initialize 
+
 
 ser = sr.Serial('/dev/ttyUSB0',9600)
 	
@@ -22,12 +53,19 @@ ser.reset_output_buffer()
 
 print("Initialized !")
 
+
+
 while 1:
 	
 	# check if the READ_SIGN signal is here
 	if( int.from_bytes(ser.read(1), byteorder = 'little') == READ_SIGN ):
 		
-		#read_sign()
+		print(" Reading sign...")
+		
+		out = read_sign().decode("utf-8")
+		
+		print("Serial Sent:" +  out)
 		
 	
-	
+
+
